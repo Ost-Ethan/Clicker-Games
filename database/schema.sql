cec6fbfadc0a498d2f5f36488f86718166556486
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-11-16 00:22:26 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,26 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" text UNIQUE,
+  "password" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "times" (
+  "timeId" serial PRIMARY KEY,
+  "userId" integer,
+  "gameId" integer,
+  "bestTime" integer
+);
+
+CREATE TABLE "games" (
+  "gameId" serial PRIMARY KEY,
+  "gameName" text
+);
+
+ALTER TABLE "times" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "times" ADD FOREIGN KEY ("gameId") REFERENCES "games" ("gameId");
